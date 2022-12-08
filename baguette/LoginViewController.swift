23f720b364457e77d2067500605f336e389385b0
bluepark <bcm.bluepark@gmail.com-at-125864053426>
2022-12-08 21:48:31 +0900

    [init] 로그인 화면 생성

---
+++ baguette/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  baguette
+//
+//  Created by blue park on 2022/12/08.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+    
+    @IBOutlet weak var btnForKakao: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    
+    }
+    
+    
+    @IBAction func goToKakaoLogin(_ sender: Any) {
+    }
+}
+
